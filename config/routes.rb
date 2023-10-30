511a9cd5e845f87721c450ab081d1d2a80bd0896
Maung Naing <maungnaing1337@gmail.com>
2023-10-30 15:47:34 -0700

    Add sessions controller & sign in link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root "movies#index"
+
   resources :users 
   get "signup" => "users#new"
   
-  root "movies#index"
+  resource :session, only: [:new, :create, :destroy]
+  get "signin" => "sessions#new"
 
   resources :movies do 
     resources :reviews
